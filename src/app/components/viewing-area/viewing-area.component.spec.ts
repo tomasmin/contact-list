5902837b29f1e1e4455c70dcaa98b965d4c74239
tommin <tomas.mineika@outlook.com>
2022-05-14 18:09:40 +0300

    fix unit tests

--- src/app/components/viewing-area/viewing-area.component.spec.ts
+++ src/app/components/viewing-area/viewing-area.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewingAreaComponent } from './viewing-area.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ViewingAreaComponent', () => {
   let component: ViewingAreaComponent;
@@ -8,9 +9,9 @@ describe('ViewingAreaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewingAreaComponent ]
-    })
-    .compileComponents();
+      declarations: [ViewingAreaComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/services/contacts.service.spec.ts
+++ src/app/services/contacts.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ContactsService } from './contacts.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ContactsService', () => {
   let service: ContactsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ContactsService);
   });
 
